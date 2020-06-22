e55c2c732d6f06c05674ade43e1cf8208f3bc406
Carl Wilke <carlwilke@outlook.com>
2020-06-22 14:36:50 +0100

    Add project files.

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Gunter Bratwurst", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+            account.MakeWithdrawal(20, DateTime.Now, "Coffee Mug");
+            account.MakeWithdrawal(25, DateTime.Now, "Instant Coffee");
+            account.MakeWithdrawal(20, DateTime.Now, "Sugar");
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
